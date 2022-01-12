46a8a7a022343b595cb30d5b8f3671429711aca9
mrweikiat <danielgohweikiat@yahoo.com>
2022-01-12 10:26:58 +0800

    Add Ninja Level 12

---
+++ Exercises/Ninja Level 12/ex1/dog/main.go
@@ -0,0 +1,7 @@
+// Package dog allows us to understand dog age
+package dog
+
+// Years converts human age to dog age
+func Years(n int) int {
+	return n * 10
+}
---
+++ Exercises/Ninja Level 12/ex1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type canine struct {
+	name string
+	age  int
+}
+
+func main() {
+	alan := canine{
+		name: "Alan",
+		age:  dog.Years(10),
+	}
+	fmt.Println(alan)
+}
